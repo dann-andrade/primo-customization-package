18221a8381c90cd565b63a423986545e0aa1c2e5
Daniel Andrade <dandrade@brocku.ca>
2022-01-07 09:50:14 -0500

    Upload original packages

---
+++ 01OCUL_BU-BU_DEFAULT/js/custom.js
@@ -0,0 +1,74 @@
+/************* LibraryH3lp code STARTS here **********/
+
+// create a function to toggle askus popout
+function askus_popout_toggle() {
+  var askus = document.getElementById('askus-popout');
+  var state = askus.getAttribute('data-askus-popout-state');
+  console.log(state)
+
+  if (state == "collapsed") {        
+      askus.setAttribute('data-askus-popout-state', 'expanded');
+  }
+  else {        
+      askus.setAttribute('data-askus-popout-state', 'collapsed');
+  }
+}
+
+/************* LibraryH3lp code ends here **********/
+
+
+
+(function(){
+"use strict";
+'use strict';
+
+// start of ANGULAR CODE
+var app = angular.module('viewCustom', ['angularLoad']);
+
+/*----------below is the code for libraryh3lp on Homepage ONLY -----------*/
+var s=document.createElement('script');
+s.id='localScript';
+s.src= 'https://ca.libraryh3lp.com/js/libraryh3lp.js?526';
+document.body.appendChild(s);
+
+app.component('prm-search-after', {
+   template: '<div id="needs-js"></div>'
+});
+/*---------------libraryh3lp code ends here---------------*/
+
+  /************* LibraryH3lp code STARTS here **********/
+  
+  /* need to import some monitoring code */
+  var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
+  x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "ca.libraryh3lp.com/js/libraryh3lp.js?526";
+  var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
+
+  var askus = document.createElement('div');
+  askus.id = 'askus-popout';
+  askus.setAttribute('data-askus-popout-state', 'collapsed'); 
+  askus.innerHTML = '<a class="tab-head" href="javascript:void()" ><img src="custom/01OCUL_BU-BU_DEFAULT/img/speech-bubble_white-fill-small.png" alt="">Ask a Librarian</a> \
+  <div class="askus_tab"> \
+  <!--<h2 class="askus_centre">Chat With Us</h2>--><!--Chat Button BEGIN--> \
+      <div class="needs-js">chat loading...</div> \
+      <ul> \
+          <li>Click the image to chat with us</li> \
+          <li>Ask Chat is a <a href="http://vr.scholarsportal.info/ask/">collaborative service</a></li> \
+          <li><a href="https://brocku.ca/library/help/chat/">Ask Chat hours</a></li> \
+          <li><a href="https://brocku.ca/library/contact/">Contact Us</a></li> \
+      </ul> \
+  </div>';    
+  document.body.appendChild(askus);
+
+  ///register the onclick for chat
+  var askus_popout = document.querySelector('#askus-popout');
+
+  var askus_onclick_att = document.createAttribute("onclick");
+  askus_onclick_att.value = "askus_popout_toggle();"; 
+  askus_popout.setAttributeNode(askus_onclick_att);
+  
+  /************* LibraryH3lp code ends here **********/
+
+
+
+
+})();
