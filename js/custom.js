99baa762ccaa66806b8a42fb1631a4604c105902
Daniel Andrade <dandrade@brocku.ca>
2023-08-25 21:51:06 -0400

    Re-added sample list for demo purposes

--- js/custom.js
+++ js/custom.js
@@ -210,7 +210,259 @@ templateUrl: 'custom/BU_DEFAULT/html/reportproblem.html',
   // Declare full books array outside the controller is the array is not destroyed 
   // with the component by ng-if
   var newbooks = new Array();
-    
+
+  newbooks = [
+    {
+      "isbn": "9781773630366",
+      "mmsid": "991009415524405152",
+      "title": "Sick and tired: health and safety inequalities"
+    },
+    {
+      "isbn": "0369103858",
+      "mmsid": "991009416423005152",
+      "title": "Bluebirds"
+    },
+    {
+      "isbn": "0369103734",
+      "mmsid": "991009416423105152",
+      "title": "Is my microphone on?"
+    },
+    {
+      "isbn": "0369103386",
+      "mmsid": "991009416423205152",
+      "title": "Every day she rose"
+    },
+    {
+      "isbn": "0369103416",
+      "mmsid": "991009416423305152",
+      "title": "Let's run away : four stories and a message for the world written in response to an unpublished memoir"
+    },
+    {
+      "isbn": "0369103637",
+      "mmsid": "991009416423405152",
+      "title": "Blow wind"
+    },
+    {
+      "isbn": "0369103505",
+      "mmsid": "991009416423505152",
+      "title": "Women of the fur trade"
+    },
+    {
+      "isbn": "0369103572",
+      "mmsid": "991009416423605152",
+      "title": "Public enemy"
+    },
+    {
+      "isbn": "0369103548",
+      "mmsid": "991009416423705152",
+      "title": "Lady sunrise"
+    },
+    {
+      "isbn": "0369103475",
+      "mmsid": "991009416423805152",
+      "title": "Everybody just c@lm the f#ck down"
+    },
+    {
+      "isbn": "036910157X",
+      "mmsid": "991009416423905152",
+      "title": "Forget me not"
+    },
+    {
+      "isbn": "9781773635545",
+      "mmsid": "991009416424005152",
+      "title": "Country of poxes : three germs and the taking of territory"
+    },
+    {
+      "isbn": "1773635522",
+      "mmsid": "991009416424105152",
+      "title": "Abolitionist intimacies"
+    },
+    {
+      "isbn": "9781773635613",
+      "mmsid": "991009416424205152",
+      "title": "Solidarity beyond bars : unionizing prison labour"
+    },
+    {
+      "isbn": "3110275686",
+      "mmsid": "991009089272105152",
+      "title": "Methods in contemporary linguistics"
+    },
+    {
+      "isbn": "311046408X",
+      "mmsid": "991009090584005152",
+      "title": "Public History and School : International Perspectives"
+    },
+    {
+      "isbn": "3110461285",
+      "mmsid": "991009091193105152",
+      "title": "Coolies of capitalism : Assam tea and the making of Coolie labour"
+    },
+    {
+      "isbn": "348698960X",
+      "mmsid": "991009093667805152",
+      "title": "Auf dem Weg zur Normalit\u00e4t : Konflikt und Verst\u00e4ndigung in den deutsch-franz\u00f6sischen Beziehungen der 1970er Jahre"
+    },
+    {
+      "isbn": "3110478129",
+      "mmsid": "991009096192605152",
+      "title": "Katechismus-Andachten (1656)"
+    },
+    {
+      "isbn": "3110579170",
+      "mmsid": "991009097033905152",
+      "title": "The Civilising Offensive : Social and educational reform in 19th century Belgium"
+    },
+    {
+      "isbn": "3110579200",
+      "mmsid": "991009098808705152",
+      "title": "Victimhood and acknowledgment : the other side of terrorism"
+    },
+    {
+      "isbn": "3110518015",
+      "mmsid": "991009099102505152",
+      "title": "Jeux de mots et cr\u00e9ativit\u00e9 : Langue(s), discours et litt\u00e9rature"
+    },
+    {
+      "isbn": "3110322501",
+      "mmsid": "991009100798105152",
+      "title": "Johanssonian investigations essays in honour of Ingvar Johansson on his seventieth birthday"
+    },
+    {
+      "isbn": "3110610477",
+      "mmsid": "991009102807905152",
+      "title": "Global histories of work"
+    },
+    {
+      "isbn": "3110262444",
+      "mmsid": "991009103625605152",
+      "title": "Our Neighbours, Ourselves : Contemporary Reflections on Survival"
+    },
+    {
+      "isbn": "3662488477",
+      "mmsid": "991009240251705152",
+      "title": "Autonomous Driving Technical, Legal and Social Aspects"
+    },
+    {
+      "isbn": "3110541572",
+      "mmsid": "991009240799405152",
+      "title": "Manuscripts and Archives : Comparative Views on Record-Keeping"
+    },
+    {
+      "isbn": "3110350548",
+      "mmsid": "991009240808905152",
+      "title": "150 Jahre deutsche Verwaltungsgerichtsbarkeit : Vortrag, gehalten vor der Juristischen Gesellschaft zu Berlin am 9. Oktober 2013 im OVG Berlin-Brandenburg"
+    },
+    {
+      "isbn": "1283166240",
+      "mmsid": "991009240945405152",
+      "title": "Heinrich Heine - ein deutscher Europ\u00e4er im franz\u00f6sischen Exil : Vortrag, gehalten vor der Juristischen Gesellschaft zu Berlin am 9. Dezember 2009"
+    },
+    {
+      "isbn": "1283629232",
+      "mmsid": "991009240964805152",
+      "title": "Grundrechtsschutz zwischen Karlsruhe und Stra\u00dfburg : Vortrag, gehalten vor der Juristischen Gesellschaft zu Berlin am 13. Juli 2011"
+    },
+    {
+      "isbn": "3110397315",
+      "mmsid": "991009241244205152",
+      "title": "Inszenierte Moderne : Popul\u00e4res Theater in Berlin und London, 1880-1930"
+    },
+    {
+      "isbn": "3110341387",
+      "mmsid": "991009241253105152",
+      "title": "Zukunftsgestaltung durch \u00d6ffentliches Recht : Referate und Diskussionen auf der Tagung der Vereinigung der Deutschen Staatsrechtslehrer in Greifswald vom 2. bis 5. Oktober 2013"
+    },
+    {
+      "isbn": "3110216949",
+      "mmsid": "991009241269305152",
+      "title": "Von Kant zu Schelling : Die beiden Wege des Deutschen Idealismus"
+    },
+    {
+      "isbn": "3110466643",
+      "mmsid": "991009241305605152",
+      "title": "Okkultismus im Geha\u0308use : Institutionalisierungen der Parapsychologie im 20. Jahrhundert im internationalen Vergleich"
+    },
+    {
+      "isbn": "311040785X",
+      "mmsid": "991009241334405152",
+      "title": "Menschen z\u00e4hlen : Wissensproduktion durch britische Volksz\u00e4hlungen und Umfragen vom 19. Jahrhundert bis ins digitale Zeitalter"
+    },
+    {
+      "isbn": "3110424711",
+      "mmsid": "991009241346905152",
+      "title": "50 Jahre Aktiengesetz"
+    },
+    {
+      "isbn": "311043346X",
+      "mmsid": "991009241366105152",
+      "title": "Brauchen wir ein drittes Geschlecht? : Reformbedarf im deutschen (Familien-)Recht nach Einf\u00fchrung des \u00a7 22 Abs. 3 PStG"
+    },
+    {
+      "isbn": "3110466961",
+      "mmsid": "991009241412305152",
+      "title": "Genre und Gemeinsinn : Hollywood zwischen Krieg und Demokratie"
+    },
+    {
+      "isbn": "3110479435",
+      "mmsid": "991009241441205152",
+      "title": "Der Prozess Jesu- aus ro\u0308misch-rechtlicher Perspektive"
+    },
+    {
+      "isbn": "3110435217",
+      "mmsid": "991009241446205152",
+      "title": "Verfassung als Ordnungskonzept : Berichte und Diskussionen auf der Tagung der Vereinigung der Deutschen Staatsrechtslehrer in Speyer vom 7.-10. Oktober 2015"
+    },
+    {
+      "isbn": "3110405555",
+      "mmsid": "991009241447405152",
+      "title": "Monastische Kultur als transkonfessionelles Ph\u00e4nomen : Beitr\u00e4ge einer deutsch-russischen interdisziplin\u00e4ren Tagung in Vladimir und Suzdal\u2019"
+    },
+    {
+      "isbn": "3110399474",
+      "mmsid": "991009241473105152",
+      "title": "Von der D\u00e4monologie zum Unbewussten : Die Transformation der Anthropologie um 1800"
+    },
+    {
+      "isbn": "1283430282",
+      "mmsid": "991009241488905152",
+      "title": "Figuren des Messianischen in Schriften deutsch-j\u00fcdischer Intellektueller 1900-1933"
+    },
+    {
+      "isbn": "3110431149",
+      "mmsid": "991009241707905152",
+      "title": "NICHTS NEUES SCHAFFEN : Perspektiven auf die treue Kopie 1300-1900"
+    },
+    {
+      "isbn": "3110579758",
+      "mmsid": "991009241729805152",
+      "title": "Disruption in the Arts : Textual, Visual, and Performative Strategies for Analyzing Societal Self-Descriptions"
+    },
+    {
+      "isbn": "3110543893",
+      "mmsid": "991009241763705152",
+      "title": "Kant on love"
+    },
+    {
+      "isbn": "3110620359",
+      "mmsid": "991009241829605152",
+      "title": "Nietzsche, Religion, and Mood"
+    },
+    {
+      "isbn": "3110424703",
+      "mmsid": "991009241898805152",
+      "title": "Handbook the global history of work"
+    },
+    {
+      "isbn": "3110575590",
+      "mmsid": "991009241925905152",
+      "title": "Studies on Greek and Coptic Majuscule Scripts and Books"
+    },
+    {
+      "isbn": "3110645629",
+      "mmsid": "991009241955005152",
+      "title": "Infrastrukturen"
+    }]
+  
   app.controller('prmSearchBarAfterController', ['$http', '$scope', 
     function ($http, $scope) {
 
@@ -233,7 +485,7 @@ templateUrl: 'custom/BU_DEFAULT/html/reportproblem.html',
           setTimeout(() =>  {  
             document.getElementById('bu-outer-carousel').scrollLeft = 0;
           },0 );
-          if ((window.location.href.startsWith("http://10.20.124.65:8003/discovery/search?vid=01OCUL_BU:BU_TEST_DAN")) 
+          if ((window.location.href.startsWith("http://10.20.124.65:8003/discovery/search?vid=01OCUL_BU:BU_NEW_TITLES")) 
               && (window.location.href.indexOf("&mode=advanced") == -1)) {        
             getBooks();
           } else {
@@ -241,27 +493,27 @@ templateUrl: 'custom/BU_DEFAULT/html/reportproblem.html',
           };
       };
 
-      // Load books from data file if not already loaded. If there is a problem hide the feature. 
+      //Load books from data file if not already loaded. If there is a problem hide the feature. 
       function getBooks() {
-          if(newbooks.length == 0 ) {
-              $http.get('https://library.brocku.ca/phpmyadmin/newtitles/gtitles.json'
-              ).then(
-                  function successCallback(data) {
-                      newbooks = data.data;
-                      loadBooks(19);
-						          findWidth();
-                  },
-                  function errorCallback(data){
-                      $ctrl.showDisplay = false;
-                      console.log(data);
-                  } 
-              );
-          } else {
-            setTimeout(() =>  {
-              loadBooks(19);
-              findWidth();
-            }, 10);
-          };
+      //     if(newbooks.length == 0 ) {
+      //         $http.get('https://library.brocku.ca/phpmyadmin/newtitles/gtitles.json'
+      //         ).then(
+      //             function successCallback(data) {
+      //                 newbooks = data.data;
+      //                 loadBooks(19);
+			// 			          findWidth();
+      //             },
+      //             function errorCallback(data){
+      //                 $ctrl.showDisplay = false;
+      //                 console.log(data);
+      //             } 
+      //         );
+      //     } else {
+      //       setTimeout(() =>  {
+      //         loadBooks(19);
+      //         findWidth();
+      //       }, 10);
+      //     };
           loadBooks(19);
           findWidth();
       };
